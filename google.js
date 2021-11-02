395fb3a35bde7aef669e3982eb17341ddf61f999
dhend595 <dhend595@gmail.com>
2021-11-01 23:44:13 -0400

    deck test

---
+++ google.js
@@ -0,0 +1,30 @@
+class HighWaterMark {
+    constructor(size) {
+        this.size = size;
+        this.buffer = [];
+        
+    }
+    
+    get HighWaterMark() {
+        return this.size;
+    }
+
+    ack (itemid) {
+        this.buffer.push(itemid);
+    }
+
+    get () {
+        return this.buffer.shift();
+    }
+    
+
+}
+
+class WaterMark {
+    constructor(size) {
+        this.size = size;
+        this.buffer = [];
+    }
+    acks (itemid) {
+        this.buffer.push(itemid);
+    }
